c1d6ddc6cdb8656a72dbe0781ce3fc0fa4dfb175
Emir Bosques <emirbosques@gmail.com>
2023-02-14 23:55:25 +0100

    Firebase Lib

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ingresoegresoredux-4a94b',
+    appId: '1:84351382337:web:ffd214c161da86ea0167c0',
+    storageBucket: 'ingresoegresoredux-4a94b.appspot.com',
+    apiKey: 'AIzaSyA2FbMo0i33WJEo_rc7_5ixc3ujO2pNnJA',
+    authDomain: 'ingresoegresoredux-4a94b.firebaseapp.com',
+    messagingSenderId: '84351382337',
+  },
   production: false
 };
 
