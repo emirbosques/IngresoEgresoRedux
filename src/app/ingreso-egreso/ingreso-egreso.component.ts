9953f05d7992ea33cb6073bb6ac0d8e6c2f204a9
Emir Bosques <emirbosques@gmail.com>
2023-03-09 23:08:11 +0100

    Ingreso Egreso Loading en button

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,22 +1,30 @@
+import { isLoading, stopLoading } from './../shared/ui.actions';
+import { AppState } from './../app.reducer';
 import { IngresoEgresoInterface } from './../models/ingreso-egreso.model';
 import { IngresoEgresoService } from './../services/ingreso-egreso.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
   styles: []
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
 
   ingresoEgresoForm!: FormGroup;
   typeButton: string = 'ingreso';
+  loading: boolean = false;
+  subscriptionStore!: Subscription;
+
 
   constructor(
     private ingresoEgresoService: IngresoEgresoService,
+    private store: Store<AppState>,
     private formBuilder: FormBuilder) {
 
   }
@@ -29,18 +37,28 @@ export class IngresoEgresoComponent implements OnInit {
       tipo: ['']
     });
 
+    this.subscriptionStore.add(this.store.select('ui')
+      .subscribe(({ isLoading }) => this.loading = isLoading));
+
     this.tipoControl?.setValue(this.typeButton);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionStore.unsubscribe();
   }
 
 
   addIngresoEgresoRecord() {
+    this.store.dispatch(isLoading());
     const ingresoEgresoData: IngresoEgresoInterface = this.ingresoEgresoForm.value;
     this.ingresoEgresoService.crearIngresoEgreso(ingresoEgresoData)
-      .then((ref) => {
-        console.log('IG-component OK', ref);
-        this.resetFormIngresoEgreso();
-        Swal.fire('Hey user!', 'Registro Correcto', 'info');
+    .then((ref) => {
+      console.log('IG-component OK', ref);
+      this.resetFormIngresoEgreso();
+      this.store.dispatch(stopLoading());
+      Swal.fire('Hey user!', 'Registro Correcto', 'info');
       }).catch((err) => {
+        this.store.dispatch(stopLoading());
         Swal.fire('Hey user!', 'Registro Incorrecto', 'error');
         console.log('IG-component KO', err);
       });
